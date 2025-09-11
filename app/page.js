bf30e04f222bcc8c99bbbbe61f7fc923840d0916
Hamim <hamimc232@gmail.com>
2025-09-11 12:37:21 -0400

    made the home page + responsive

--- app/page.js
+++ app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Typography, Box, Button } from "@mui/material";
+import "@fontsource/bitcount-grid-double";
 
-export default function Home() {
+const floatingWords = [
+  "I'm about to dog this down.",
+  "Let's see how expensive...",
+  "Alright, I just got out of work...",
+  "Fine Dining.",
+  "Michelin Starred Restaurant.",
+  "Thank you.",
+  "Leave a tip.",
+  "This smells good",
+  "10 out of 10",
+  "9 out of 10",
+  "Oh wow, this is amazing.",
+  "",
+];
+
+export default function HomePage() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: { xs: "center", md: "center" },
+        alignItems: { xs: "center", md: "center" },
+        height: "100vh",
+        background: "black",
+        textAlign: { xs: "center", md: "center" },
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{
+          fontFamily: "Bitcount Grid Double",
+          fontWeight: "bold",
+          color: "#fbb833	",
+          justifyContent: { xs: "center", md: "center" },
+          mx: "auto",
+        }}
+      >
+        Ertan Eats
+      </Typography>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Button
+        type="a"
+        href="/locations"
+        variant="contained"
+        sx={{
+          fontFamily: "Bitcount Grid Double",
+          fontSize: "23px",
+          background: "#1e3d6d",
+          marginTop: "10px",
+        }}
+      >
+        Enter Here
+      </Button>
+    </Box>
   );
 }
