3f3d61f06187dc5e94b78aa7d5f0138647caeb5c
Hamim <hamimc232@gmail.com>
2025-10-04 21:38:16 -0400

    minor cleaning up + changes to card

--- app/locations/page.js
+++ app/locations/page.js
@@ -6,25 +6,28 @@ export default async function LocationsPage() {
   const { data: restaurant, error } = await supabase
     .from("restaurant")
     .select("*");
-
-  console.log("restaurants:", restaurant, "error:", error);
   if (error) {
     console.error("Supabase fetch error:", error);
     return <p>Error loading restaurants.</p>;
   }
 
   return (
+    <>
     <div
       style={{
         display: "grid",
         gap: "1.5rem",
         gridTemplateColumns: "repeat(auto-fill, minmax(400px, 1fr))",
         padding: "2rem",
+        backgroundColor: "black",
+        paddingTop: "200px"
       }}
     >
       {restaurant?.map((r) => (
-        <RestaurantCard key={r.id} data={r} />
+        <RestaurantCard key={r.id} data={r} sx = {{
+        }} />
       ))}
     </div>
+    </>
   );
 }
