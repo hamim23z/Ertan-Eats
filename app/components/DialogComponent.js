0688403b8154cd9f4ea6204be800175f572e32ef
Hamim <hamimc232@gmail.com>
2025-10-19 21:21:54 -0400

    Working on embedding YT shorts. Already have data in DB

--- app/components/DialogComponent.js
+++ app/components/DialogComponent.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { useState } from "react";
 import {
   Box,
-  Button,
   Typography,
   Dialog,
-  DialogActions,
   DialogContent,
   DialogTitle,
   DialogContentText,
@@ -13,10 +10,69 @@ import {
 
 export default function DialogCard({ open, handleClose, data }) {
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Testing to see if Dialog actually opens.</DialogTitle>
+    <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
+      <DialogTitle
+        variant="h4"
+        sx={{
+          textAlign: "center",
+          fontFamily: "Bitcount Grid Double",
+          fontWeight: "bold",
+        }}
+      >
+        Video + Food Ordered
+      </DialogTitle>
+
+      <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}>
+        {/**left side of screen */}
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: { xs: "center", md: "left" },
+            px: 2,
+            py: { xs: 10, md: 0 },
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{ fontFamily: "Bitcount Grid Double", fontWeight: "bold" }}
+          >
+            Instagram Video
+          </Typography>
+        </Box>
+
+        {/**right side of screen */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: { xs: "center", md: "flex-start" },
+            justifyContent: "center",
+            width: { xs: "100%", md: "40%" },
+            pl: { md: 2 },
+            gap: 2,
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              justifyContent: "center",
+              fontFamily: "Bitcount Grid Double",
+              fontWeight: "bold",
+            }}
+          >
+            Food Ordered
+          </Typography>
+
+          {/**in here will probably be another database listing what was ordered + ratings */}
+        </Box>
+      </Box>
+
       <DialogContent>
-        <DialogContentText>Test test 123</DialogContentText>
+        <DialogContentText></DialogContentText>
       </DialogContent>
     </Dialog>
   );
---
+++ app/components/DisplayVideo.js
@@ -0,0 +1,4 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Card } from "@mui/material";
+import { createClient } from "../utils/supabase/client";
